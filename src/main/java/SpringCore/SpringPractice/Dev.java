4d3f15952ce7412cb2e6e2c024a658c7bd53d27b
Sampannasapkota <henryatokpas12@gmail.com>
2025-08-31 00:07:19 +0545

    dependency injection through java core

---
+++ src/main/java/SpringCore/SpringPractice/Dev.java
@@ -0,0 +1,10 @@
+package SpringCore.SpringPractice;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Dev {
+    public void build(){
+        System.out.println("We are doing awesome projects.");
+    }
+}
--- src/main/java/SpringCore/SpringPractice/SpringPracticeApplication.java
+++ src/main/java/SpringCore/SpringPractice/SpringPracticeApplication.java
@@ -2,12 +2,21 @@ package SpringCore.SpringPractice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class SpringPracticeApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringPracticeApplication.class, args);
+//context obj
+		ApplicationContext context= SpringApplication.run(SpringPracticeApplication.class);
+		//This is old method i.e core java method
+//		Dev dev=new Dev();
+
+
+		//but in spring we have dependency injection through context.getBean(--classname--);
+		Dev dev= context.getBean(Dev.class);
+		dev.build();
 	}
 
 }
